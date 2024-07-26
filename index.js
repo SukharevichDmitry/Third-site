bb3563af3f700e8d408c4eab5e0d82a2f5b3d608
Sukharevich Dmitry <defgolem@gmail.com>
2024-07-26 21:56:49 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+    function showLoadingScreen() {
+      document.getElementById('loading-screen').style.display = 'flex';
+      window.location.href = 'https://dmitrysukharevich.pipedrive.com/deal/19';
+    }
