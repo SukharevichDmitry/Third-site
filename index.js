c3a07d676683cc5a404eba829af90d1d8aa55bcf
Sukharevich Dmitry <defgolem@gmail.com>
2024-07-27 12:10:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-    function showLoadingScreen() {
-      document.getElementById('loading-screen').style.display = 'flex';
-      window.location.href = 'https://dmitrysukharevich.pipedrive.com/deal/19';
+function showLoadingScreen(pipedriveUrl) {
+    document.getElementById('loading-screen').style.display = 'flex';
+    window.location.href = pipedriveUrl;
+}
+
+window.onload = function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const pipedriveUrl = urlParams.get('url');
+
+    if (pipedriveUrl) {
+        document.getElementById('view-details-link').addEventListener('click', function() {
+            showLoadingScreen(pipedriveUrl);
+        });
+    } else {
+        console.error('URL Pipedrive не найден.');
     }
+};
